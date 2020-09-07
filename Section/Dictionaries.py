2120bf8de4bb360a67a9187faceb249f5bdd55b6
BSchubel <echoiks@gmail.com>
2020-09-06 22:08:12 -0600

    Dictionaries
    Learning how to put together a dictionary and use keys to find a value.

---
+++ Section/Dictionaries.py
@@ -0,0 +1,19 @@
+
+monthConversions = {
+    "Jan": "January",
+    "Feb": "February",
+    "Mar": "March",
+    "Apr": "April",
+    "May": "May",
+    "Jun": "June",
+    "Jul": "July",
+    "Aug": "August",
+    "Sep": "September",
+    "Oct": "October",
+    "Nov": "November",
+    "Dec": "December",
+}
+
+print(monthConversions["Mar"])
+#can also use print(monthConversions.get("Luv", "Not a valid key")) to get a default value
+#Learning how to put together a dictionary and use the key to find a value
